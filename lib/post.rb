d019b8266dc98094eeeb0005b84ef5fa92653065
jeremylenz <jeremylenz@gmail.com>
2017-06-09 15:35:30 -0400

    song count

---
+++ lib/post.rb
@@ -0,0 +1,17 @@
+class Post
+
+attr_accessor :title, :author
+
+def initialize(title)
+  @title = title
+end
+
+def author_name
+  return nil if @author == nil
+  @author.name
+end
+
+
+
+
+end #of Post class
